3cc8ade02a4f4b15b919fd3e28b07483ffc85c44
Messias Junior <messias.junior@tui.co.uk>
2021-05-24 21:51:18 +0100

    feat: restore selected color when device rotate

--- app/src/main/java/com/messias/gazetadev/MainActivity.kt
+++ app/src/main/java/com/messias/gazetadev/MainActivity.kt
@@ -3,35 +3,50 @@ package com.messias.gazetadev
 import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.messias.gazetadev.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initViews()
+        initObservers()
+    }
+
+    private fun initViews() {
         setupBottomNavigation()
     }
 
     private fun setupBottomNavigation() {
         binding.bottomNavigation.setOnNavigationItemSelectedListener { menuItem ->
-            updateBottomNavigationColor(menuItem)
+            viewModel.onItemSelected(menuItem.itemId)
             true
         }
     }
 
-    private fun updateBottomNavigationColor(menuItem: MenuItem) {
-        val itemColor = getItemColor(menuItem)
+    private fun initObservers() {
+        initItemSelectedObserver()
+    }
+
+    private fun initItemSelectedObserver() {
+        viewModel.selectedItem.observe(this) { updateBottomNavigationColor(it) }
+    }
+
+    private fun updateBottomNavigationColor(itemId: Int) {
+        val itemColor = getItemColor(itemId)
         binding.bottomNavigation.itemIconTintList = ColorStateList.valueOf(itemColor)
         binding.bottomNavigation.itemTextColor = ColorStateList.valueOf(itemColor)
     }
 
-    private fun getItemColor(menuItem: MenuItem): Int {
-        val colorRes = when(menuItem.itemId) {
+    private fun getItemColor(itemId: Int): Int {
+        val colorRes = when (itemId) {
             R.id.main_menu_all -> R.color.all
             R.id.main_menu_youtube -> R.color.youtube
             R.id.main_menu_articles -> R.color.articles
---
+++ app/src/main/java/com/messias/gazetadev/MainViewModel.kt
@@ -0,0 +1,14 @@
+package com.messias.gazetadev
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    private val _selectedItem = MutableLiveData<Int>()
+    val selectedItem: LiveData<Int> = _selectedItem
+
+    fun onItemSelected(itemId: Int) {
+        _selectedItem.value = itemId
+    }
+}
