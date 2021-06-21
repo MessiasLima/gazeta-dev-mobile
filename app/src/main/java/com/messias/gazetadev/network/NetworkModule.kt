163192e7449f2aade60163029f3f5f1391985042
Messias Junior <messias.junior@tui.co.uk>
2021-06-21 21:13:45 +0100

    feat: enable cache

--- app/src/main/java/com/messias/gazetadev/network/NetworkModule.kt
+++ app/src/main/java/com/messias/gazetadev/network/NetworkModule.kt
@@ -1,9 +1,13 @@
 package com.messias.gazetadev.network
 
+import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -11,15 +15,24 @@ import retrofit2.converter.gson.GsonConverterFactory
 @InstallIn(SingletonComponent::class)
 class NetworkModule {
     @Provides
-    fun provideGazetaDevApi(): GazetaDevApi {
+    fun provideCache(@ApplicationContext context: Context) =
+        Cache(context.cacheDir, CACHE_SIZE.toLong())
+
+    @Provides
+    fun provideHttpClient(cache: Cache) = OkHttpClient.Builder().cache(cache).build()
+
+    @Provides
+    fun provideGazetaDevApi(okHttpClient: OkHttpClient): GazetaDevApi {
         return Retrofit.Builder()
             .baseUrl(GAZETA_DEV_API_HOST)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
             .build()
             .create(GazetaDevApi::class.java)
     }
 
     companion object {
+        private const val CACHE_SIZE = 10 * 1024 * 1024
         private const val GAZETA_DEV_API_HOST = "https://gazeta.dev"
     }
 }
